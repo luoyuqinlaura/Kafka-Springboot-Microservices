57243a119208b2098e91955255322280c076c59b
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-03-31 15:39:36 -0700

    Producer config and create a topic

---
+++ src/main/java/com/example/springkafkawiki/SpringKafkaWikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaWikiApplication.class, args);
+	}
+
+}
