1c8ea66cb9609eaeda0334ce41d416ddacfbf700
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-04-01 09:45:57 -0700

    connect with DB

--- consumer/src/main/java/springboot/KafkaDBConsumer.java
+++ consumer/src/main/java/springboot/KafkaDBConsumer.java
@@ -4,13 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import springboot.entity.WikimediaData;
+import springboot.repository.WikimediaDataRepository;
 
 @Service
 public class KafkaDBConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDBConsumer.class);
+
+    private WikimediaDataRepository wikimediaDataRepository;
+
+    public KafkaDBConsumer(WikimediaDataRepository wikimediaDataRepository) {
+        this.wikimediaDataRepository = wikimediaDataRepository;
+    }
+
     @KafkaListener(topics = "wikimedia_recentchange",groupId = "myGroup")
     public void consume(String eventMessage) {
         LOGGER.info(String.format("Message received -> %s", eventMessage));
+
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+//        wikimediaDataRepository.save(wikimediaData);
     }
 }
